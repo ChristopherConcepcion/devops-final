03b2e8c69c1723812f62c413c1f78b52fcf6aa9b
chris2xC <chaztizze@gmail.com>
2025-12-15 01:01:48 +0000

    Fix jest open handle by preventing server start in tests

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+  });
+}
+
 
 module.exports = app;
 

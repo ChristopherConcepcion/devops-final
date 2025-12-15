118bdf0335ba39f971e6a4a6ec141e73e47d0bb9
chris2xC <chaztizze@gmail.com>
2025-12-15 00:48:30 +0000

    Practica final DevOps CI/CD

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo DevOps CI/CD 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+
+module.exports = app;
